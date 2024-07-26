453a777040c7c1aee8a4b1d511d73491a249d03f
Simmo Saan <simmo.saan@gmail.com>
2024-07-26 10:45:42 +0300

    Add availability attribute with multiple dots to test

--- test/small1/attr-assign.c
+++ test/small1/attr-assign.c
@@ -5,3 +5,10 @@ void foo(void) __attribute__((availability(macos,introduced=10.15)));
 void foo(void) {
     return;
 }
+
+// Version numbers may have multiple dots: https://github.com/goblint/cil/pull/171#issuecomment-2250670652
+void bar(void) __attribute__((availability(macos,introduced=10.13.4)));
+
+void bar(void) {
+    return;
+}
